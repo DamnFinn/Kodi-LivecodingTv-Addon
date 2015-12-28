b1b5c6c27dc4611474b68572dd26f73740ecb86c
bamberino <bamberino@wir-sind-cool.org>
2015-12-28 23:56:38 +0100

    classes.py and client.py introduced

--- addon.py
+++ addon.py
@@ -1,200 +1,162 @@
 import sys
 from urlparse import parse_qsl
 import xbmcaddon, xbmcgui, xbmcplugin
-import simplejson as json
+import requests
+from classes import Livestream, Logger
+from client import LivecodingTvApp
 
 # Get the plugin url in plugin:// notation.
-_url = sys.argv[0]
+_plugin_url = sys.argv[0]
 # Get the plugin handle as an integer number.
-_handle = int(sys.argv[1])
+_plugin_handle = int(sys.argv[1])
 
 _addon = xbmcaddon.Addon()
 _app_shortname = 'LCTV'
 
-# Free sample videos are provided by www.vidsplay.com
-# Here we use a fixed set of properties simply for demonstrating purposes
-# In a "real life" plugin you will need to get info and links to video files/streams
-# from some web-site or online service.
-_mainmenu = {
-	_addon.getLocalizedString(30010): [
-	{
-		'name': 'Crab',
-		'thumb': 'http://www.vidsplay.com/vids/crab.jpg',
-		'video': 'http://www.vidsplay.com/vids/crab.mp4',
-		'genre': 'Animals'
-	},
-	{
-		'name': 'Alligator',
-		'thumb': 'http://www.vidsplay.com/vids/alligator.jpg',
-		'video': 'http://www.vidsplay.com/vids/alligator.mp4',
-		'genre': 'Animals'
-	}],
-	_addon.getLocalizedString(30011): [
-	{
-		'name': 'Crab',
-		'thumb': 'http://www.vidsplay.com/vids/crab.jpg',
-		'video': 'http://www.vidsplay.com/vids/crab.mp4',
-		'genre': 'Animals'
-	}],
-	_addon.getLocalizedString(30012): [
-	{
-		'name': 'Crab',
-		'thumb': 'http://www.vidsplay.com/vids/crab.jpg',
-		'video': 'http://www.vidsplay.com/vids/crab.mp4',
-		'genre': 'Animals'
-	}],
-	_addon.getLocalizedString(30019): [
-	{
-		'name': 'Crab',
-		'thumb': 'http://www.vidsplay.com/vids/crab.jpg',
-		'video': 'http://www.vidsplay.com/vids/crab.mp4',
-		'genre': 'Animals'
-	}]
-}
+_lctv_app = LivecodingTvApp()
+_log = Logger('addon.py')
+
+_mainmenu = [[30010, 'livestreams']]
+_action = 'action'
+_user = 'user'
 
 
-def get_categories():
-    """
-    Get the list of video categories.
-    :return: list
-    """
-    return _mainmenu.keys()
+def get_plugin_url(params):
+	url_params = ''
+	for param in params:
+		if len(url_params) == 0:
+			url_params = '?'
+		else:
+			url_params += '&'
+		url_params += '%s=%s' % (param[0], param[1])
+	requested_plugin_url = '%s%s' % (_plugin_url, url_params)
+	# logging
+	message = 'Requested plugin url: ' + requested_plugin_url
+	_log.debug(message)
+	return requested_plugin_url
 
 
-def get_videos(category):
-    """
-    Get the list of videofiles/streams.
-    Here you can insert some parsing code that retrieves
-    the list of videostreams in a given category from some site or server.
-    :param category: str
-    :return: list
-    """
-    return _mainmenu[category]
+def list_mainmenu():
+	# Create a list for our items.
+	listing = []
+	# Iterate through categories
+	for menu_item in _mainmenu:
+		menu_item_name = _addon.getLocalizedString(menu_item[0])
+		# Create a list item with a text label and a thumbnail image, if applicable
+		list_item = xbmcgui.ListItem(label=menu_item_name)
+		# Set a fanart image for the list item.
+		#list_item.setProperty('fanart_image', _mainmenu[category][0]['thumb'])
+		# Set additional info for the list item.
+		# For available properties see the following link:
+		# http://mirrors.xbmc.org/docs/python-docs/15.x-isengard/xbmcgui.html#ListItem-setInfo
+		#list_item.setInfo('video', {'title': category, 'genre': category})
+		# Create a URL for the plugin recursive callback.
+		# Example: plugin://plugin.video.example/?action=listing&category=Animals
+		url = get_plugin_url([[_action, menu_item[1]]])
+		# is_folder = True means that this item opens a sub-list of lower level items.
+		is_folder = True
+		# Add our item to the listing as a 3-element tuple.
+		listing.append((url, list_item, is_folder))
+	# Add our listing to Kodi.
+	# Large lists and/or slower systems benefit from adding all items at once via addDirectoryItems
+	# instead of adding one by ove via addDirectoryItem.
+	xbmcplugin.addDirectoryItems(_plugin_handle, listing, len(listing))
+	# Add a sort method for the virtual folder items (alphabetically, ignore articles)
+	#xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+	# Finish creating a virtual folder.
+	xbmcplugin.endOfDirectory(_plugin_handle)
 
 
-def list_categories():
-    """
-    Create the list of video categories in the Kodi interface.
-    :return: None
-    """
-    # Get video categories
-    categories = get_categories()
-    # Create a list for our items.
-    listing = []
-    # Iterate through categories
-    for category in categories:
-        # Create a list item with a text label and a thumbnail image.
-        list_item = xbmcgui.ListItem(label=category, thumbnailImage=_mainmenu[category][0]['thumb'])
-        # Set a fanart image for the list item.
-        # Here we use the same image as the thumbnail for simplicity's sake.
-        list_item.setProperty('fanart_image', _mainmenu[category][0]['thumb'])
-        # Set additional info for the list item.
-        # Here we use a category name for both properties for for simplicity's sake.
-        # setInfo allows to set various information for an item.
-        # For available properties see the following link:
-        # http://mirrors.xbmc.org/docs/python-docs/15.x-isengard/xbmcgui.html#ListItem-setInfo
-        list_item.setInfo('video', {'title': category, 'genre': category})
-        # Create a URL for the plugin recursive callback.
-        # Example: plugin://plugin.video.example/?action=listing&category=Animals
-        url = '{0}?action=listing&category={1}'.format(_url, category)
-        # is_folder = True means that this item opens a sub-list of lower level items.
-        is_folder = True
-        # Add our item to the listing as a 3-element tuple.
-        listing.append((url, list_item, is_folder))
-    # Add our listing to Kodi.
-    # Large lists and/or slower systems benefit from adding all items at once via addDirectoryItems
-    # instead of adding one by ove via addDirectoryItem.
-    xbmcplugin.addDirectoryItems(_handle, listing, len(listing))
-    # Add a sort method for the virtual folder items (alphabetically, ignore articles)
-    xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
-    # Finish creating a virtual folder.
-    xbmcplugin.endOfDirectory(_handle)
+def request_json(url):
+	data = requests.get(url, auth=(_lctv_app.client_id, _lctv_app.client_secret))
+	json = data.json()	
+	_log.debug('Answer to request from %s: %s' % (url, json))
+	return json
 
 
-def list_videos(category):
-    """
-    Create the list of playable videos in the Kodi interface.
-    :param category: str
-    :return: None
-    """
-    # Get the list of videos in the category.
-    videos = get_videos(category)
-    # Create a list for our items.
-    listing = []
-    # Iterate through videos.
-    for video in videos:
-        # Create a list item with a text label and a thumbnail image.
-        list_item = xbmcgui.ListItem(label=video['name'], thumbnailImage=video['thumb'])
-        # Set a fanart image for the list item.
-        # Here we use the same image as the thumbnail for simplicity's sake.
-        list_item.setProperty('fanart_image', video['thumb'])
-        # Set additional info for the list item.
-        list_item.setInfo('video', {'title': video['name'], 'genre': video['genre']})
-        # Set additional graphics (banner, poster, landscape etc.) for the list item.
-        # Again, here we use the same image as the thumbnail for simplicity's sake.
-        list_item.setArt({'landscape': video['thumb']})
-        # Set 'IsPlayable' property to 'true'.
-        # This is mandatory for playable items!
-        list_item.setProperty('IsPlayable', 'true')
-        # Create a URL for the plugin recursive callback.
-        # Example: plugin://plugin.video.example/?action=play&video=http://www.vidsplay.com/vids/crab.mp4
-        url = '{0}?action=play&video={1}'.format(_url, video['video'])
-        # Add the list item to a virtual Kodi folder.
-        # is_folder = False means that this item won't open any sub-list.
-        is_folder = False
-        # Add our item to the listing as a 3-element tuple.
-        listing.append((url, list_item, is_folder))
-    # Add our listing to Kodi.
-    # Large lists and/or slower systems benefit from adding all items at once via addDirectoryItems
-    # instead of adding one by ove via addDirectoryItem.
-    xbmcplugin.addDirectoryItems(_handle, listing, len(listing))
-    # Add a sort method for the virtual folder items (alphabetically, ignore articles)
-    xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
-    # Finish creating a virtual folder.
-    xbmcplugin.endOfDirectory(_handle)
+def get_livestreams():
+	livestreams_json = request_json('https://www.livecoding.tv:443/api/livestreams/onair/')
+	#iteratore through livestreams
+	livestreams = []
+	for livestream_json in livestreams_json['results']:
+		livestream = Livestream(livestream_json)
+		livestreams.append(livestream)
+	return livestreams
 
 
-def play_video(path):
-    """
-    Play a video by the provided path.
-    :param path: str
-    :return: None
-    """
-    # Create a playable item with a path to play.
-    play_item = xbmcgui.ListItem(path=path)
-    # Pass the item to the Kodi player.
-    xbmcplugin.setResolvedUrl(_handle, True, listitem=play_item)
+def list_livestreams():
+	# Get the list of videos in the category.
+	#videos = get_videos(category)
+	# Create a list for our items.
+	listing = []
+	# Iterate through videos.
+	for livestream in get_livestreams():
+		# Create a list item with a text label and a thumbnail image.
+		list_item = xbmcgui.ListItem(label=livestream.display_title, thumbnailImage=livestream.thumbnail)
+		# Set a fanart image for the list item.
+		list_item.setProperty('fanart_image', livestream.thumbnail)
+		# Set additional info for the list item.
+		list_item.setInfo('video', {'title': livestream.title, 'genre': livestream.coding_category})
+		# Set additional graphics (banner, poster, landscape etc.) for the list item.
+		list_item.setArt({'landscape': livestream.thumbnail})
+		# Set 'IsPlayable' property to 'true'.
+		# This is mandatory for playable items!
+		list_item.setProperty('IsPlayable', 'true')
+		# Create a URL for the plugin recursive callback
+		# Example: plugin://plugin.video.example/?action=play&video=http://www.vidsplay.com/vids/crab.mp4
+		url = get_plugin_url([[_action, _mainmenu[0][1]], [_user, livestream.user_slug]])
+		# Add the list item to a virtual Kodi folder.
+		# is_folder = False means that this item won't open any sub-list.
+		is_folder = False
+		# Add our item to the listing as a 3-element tuple.
+		listing.append((url, list_item, is_folder))
+	# Add our listing to Kodi.
+	# Large lists and/or slower systems benefit from adding all items at once via addDirectoryItems
+	# instead of adding one by ove via addDirectoryItem.
+	xbmcplugin.addDirectoryItems(_plugin_handle, listing, len(listing))
+	# Add a sort method for the virtual folder items (alphabetically, ignore articles)
+	#xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+	# Finish creating a virtual folder.
+	xbmcplugin.endOfDirectory(_plugin_handle)
 
+
+def watch_livestream(user_slug):
+	# Create a playable item with a path to play.
+	#play_item = xbmcgui.ListItem(path=path)
+	# Pass the item to the Kodi player.
+	#xbmcplugin.setResolvedUrl(_handle, True, listitem=play_item)
+	message = 'Livestreams cannot be watched right now'
+	_log.info(message)
+
+"""
+def show_notification_error(message):
+	_log.error(message)
+	xbmcgui.Dialog().notification(_app_shortname, message, xbmcgui.NOTIFICATION_ERROR, 15000)
+"""
 
 def router(paramstring):
-    """
-    Router function that calls other functions
-    depending on the provided paramstring
-    :param paramstring:
-    :return:
-    """
-    # Parse a URL-encoded paramstring to the dictionary of
-    # {<parameter>: <value>} elements
-    params = dict(parse_qsl(paramstring))
-
-    # Check the parameters passed to the plugin
-    if params:
-        if params['action'] == 'listing':
-            # Display the list of videos in a provided category.
-            list_videos(params['category'])
-        elif params['action'] == 'play':
-            # Play a video from a provided URL.
-            play_video(params['video'])
-    else:
-        # If the plugin is called from Kodi UI without any parameters,
-        # display the list of video categories
-        list_categories()
+	"""
+	Router function that calls other functions depending on the provided paramstring
+	:param paramstring:
+	:return:
+	"""
+	# Parse a URL-encoded paramstring to the dictionary of {<parameter>: <value>} elements
+	params = dict(parse_qsl(paramstring))
+	# Check the parameters passed to the plugin
+	if params:
+		if params[_action] == _mainmenu[0][1]:
+			if _user in params:
+				watch_livestream(params[_user])
+			else:
+				list_livestreams()
+	else:
+		# default, without any parameters
+		list_mainmenu()
 
 
 if __name__ == '__main__':
-	message = 'Starting app %s with: %s %s' % (_app_shortname, sys.argv[0], sys.argv[1])
-	xbmcgui.Dialog().notification(_app_shortname, message, xbmcgui.NOTIFICATION_INFO, 15000)
-	
+	#message = 'Starting app %s' % (_app_shortname)
+	#xbmcgui.Dialog().notification(_app_shortname, message, xbmcgui.NOTIFICATION_INFO, 10000)
 	# Call the router function and pass the plugin call parameters to it.
 	# We use string slicing to trim the leading '?' from the plugin call paramstring
 	router(sys.argv[2][1:])
---
+++ client.py
@@ -0,0 +1,4 @@
+class LivecodingTvApp(object):
+	def __init__(self):
+         self.client_id = '<livecodingtv-app-client-id>'
+         self.client_secret = '<livecodingtv-app-client-secret>'
